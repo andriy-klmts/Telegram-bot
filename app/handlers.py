821691353beff3a225956ec3f6058b5295499331
Andriy Kolomiets <andriy.klmts@gmail.com>
2024-09-22 21:15:26 +0300

    Lesson 5

--- app/handlers.py
+++ app/handlers.py
@@ -1,13 +1,13 @@
 from aiogram import  F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message,CallbackQuery
 import app.keyboards as kb
 
 rt=Router()
 
 @rt.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply('Hello!',reply_markup=await kb.keybord_buttonbuilder())
+    await message.reply('Hello!',reply_markup= kb.main)
 
 @rt.message(Command('help'))
 async def cmd_help(message:Message):
@@ -26,3 +26,9 @@ async def cmd_photo(message: Message):
 @rt.message(Command('get_photo'))
 async def cmd_get_photo(message:Message):
     await message.answer_photo(photo='AgACAgIAAxkBAAMSZu_4KM49pxAeHHjNiZaZ3_iHoowAAkLpMRugAnlLfbertm7TuTwBAAMCAANtAAM2BA',caption='Any photo.')
+
+@rt.callback_query(F.data=='one')
+async def one(callback:CallbackQuery):
+    await callback.answer('')
+    # await callback.message.answer('Hello one.',reply_markup=await kb.keybord_buttonbuilder())
+    await callback.message.edit_text('Hello one.',reply_markup=await kb.keybord_buttonbuilder())
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,18 +1,23 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup,InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder,InlineKeyboardBuilder
 
-main=ReplyKeyboardMarkup(keyboard=
-                         [
-                             [KeyboardButton(text='1')],
-                             [KeyboardButton(text='2'),KeyboardButton(text='3')]
-                             ],resize_keyboard=True)
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text="Button 1", callback_data="one"),
+        InlineKeyboardButton(text="Button 2", callback_data="two")
+    ],
+    [
+        InlineKeyboardButton(text="Button 3", callback_data="three")
+    ]
+])
 
 
 settings=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='github',url='https://github.com/andriy-klmts/Telegram-bot')]])
 
 
 async def keybord_buttonbuilder():
-    keybrd=ReplyKeyboardBuilder()
+    keybrd=InlineKeyboardBuilder()
     for tmp in ['bmw','nisan','pego']:
-        keybrd.add(KeyboardButton(text=tmp))
+        keybrd.add(InlineKeyboardButton(text=tmp,url='https://www.youtube.com/watch?v=QvENfMFhP60&list=PLV0FNhq3XMOJ31X9eBWLIZJ4OVjBwb-KM&index=5'))
     return keybrd.adjust(2).as_markup()
