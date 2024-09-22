26b7b8c0a3b472176c806e0a707b9849bb00dc3d
Andriy Kolomiets <andriy.klmts@gmail.com>
2024-09-22 14:05:38 +0300

    Lesson 2

--- run.py
+++ run.py
@@ -2,8 +2,8 @@
 
 import asyncio, logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 from config import TOKEN
@@ -18,6 +18,23 @@ dp=Dispatcher()
 async def cmd_start(message: Message):
     await message.answer('Hello!')
 
+@dp.message(Command('help'))
+async def cmd_help(message:Message):
+    await message.answer('This is command /help .')
+
+
+@dp.message(F.text=='/next')
+async def cmd_help(message:Message):
+    await message.answer('This is command /next .')
+
+@dp.message(F.photo)
+async def cmd_photo(message: Message):
+    await message.answer(f'Id photo:{message.photo[-1].file_id}')
+
+
+@dp.message(Command('get_photo'))
+async def cmd_get_photo(message:Message):
+    await message.answer_photo(photo='AgACAgIAAxkBAAMSZu_4KM49pxAeHHjNiZaZ3_iHoowAAkLpMRugAnlLfbertm7TuTwBAAMCAANtAAM2BA',caption='Any photo.')
 
 async def main():
     await dp.start_polling(bot)
