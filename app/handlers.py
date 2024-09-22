077d926908fb14bb2c03dd11d6670d6be7102433
Andriy Kolomiets <andriy.klmts@gmail.com>
2024-09-22 22:03:34 +0300

    Lesson 6

--- app/handlers.py
+++ app/handlers.py
@@ -2,8 +2,14 @@ from aiogram import  F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message,CallbackQuery
 import app.keyboards as kb
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 
 rt=Router()
+
+class Reg(StatesGroup):
+    name=State()
+    number=State()
 
 @rt.message(CommandStart())
 async def cmd_start(message: Message):
@@ -32,3 +38,23 @@ async def one(callback:CallbackQuery):
     await callback.answer('')
     # await callback.message.answer('Hello one.',reply_markup=await kb.keybord_buttonbuilder())
     await callback.message.edit_text('Hello one.',reply_markup=await kb.keybord_buttonbuilder())
+
+
+@rt.message(Command('reg'))
+async def reg_one(message:Message,state:FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer(f'Input name:{await state.get_data()}')
+
+@rt.message(Reg.name)
+async def reg_two(message: Message, state:FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer('Input number')
+
+
+@rt.message(Reg.number)
+async def reg_three(message: Message, state:FSMContext):
+    await state.update_data(number=message.text)
+    data=await state.get_data()
+    await message.answer(f' Thank! {data}')
+    await state.clear()
