04d2e3094a0560034c82a7f3e62fc398ec501abe
Andriy Kolomiets <andriy.klmts@gmail.com>
2024-09-22 13:45:18 +0300

    Lesson 1

---
+++ run.py
@@ -0,0 +1,30 @@
+#! /bin/python3
+
+import asyncio, logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from config import TOKEN
+
+
+bot= Bot(TOKEN)
+dp=Dispatcher()
+
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Hello!')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__=='__main__':
+        logging.basicConfig(level=logging.INFO)
+        try:
+            asyncio.run(main())
+        except KeyboardInterrupt:
+            print('Exit')
